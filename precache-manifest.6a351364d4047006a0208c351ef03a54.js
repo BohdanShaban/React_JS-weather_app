0cfece8b8c730e88545f2f4e95c17e8e1af9a0e9
Bohdan <bohdanshaban97@gmail.com>
2019-10-24 14:11:36 +0200

    Updates

---
+++ precache-manifest.6a351364d4047006a0208c351ef03a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdb9f46475e6c25678734b092eca66b",
+    "url": "/React_JS-weather_app/index.html"
+  },
+  {
+    "revision": "ffce1166e224808e5e5a",
+    "url": "/React_JS-weather_app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "892eb1ff241f49651a0e",
+    "url": "/React_JS-weather_app/static/css/main.2007e35f.chunk.css"
+  },
+  {
+    "revision": "ffce1166e224808e5e5a",
+    "url": "/React_JS-weather_app/static/js/2.5cf91c0c.chunk.js"
+  },
+  {
+    "revision": "892eb1ff241f49651a0e",
+    "url": "/React_JS-weather_app/static/js/main.b7c43c1b.chunk.js"
+  },
+  {
+    "revision": "74a0eeb4b0e0cd835ac8",
+    "url": "/React_JS-weather_app/static/js/runtime~main.d74e4273.js"
+  },
+  {
+    "revision": "c36cb292f5918913b0b631f47dc1c32c",
+    "url": "/React_JS-weather_app/static/media/3.c36cb292.jpg"
+  }
+]);
